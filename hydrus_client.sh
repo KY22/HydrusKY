71145f1aa30b26f1b1fcee2a36d131e3cffba968
KY22 <fatulazy@gmail.com>
2026-03-17 22:53:49 -0300

    actualizado script de lanzamiento

--- hydrus_client.sh
+++ hydrus_client.sh
@@ -3,15 +3,15 @@
 pushd "$(dirname "$0")" || exit 1
 
 if [ ! -d "venv" ]; then
-    echo "You need to set up a venv! Check the running from source help for more info!"
-    popd || exit 1
-    exit 1
+	echo "You need to set up a venv! Check the running from source help for more info!"
+	popd || exit 1
+	exit 1
 fi
 
 if ! source venv/bin/activate; then
-    echo "The venv failed to activate, stopping now!"
-    popd || exit 1
-    exit 1
+	echo "The venv failed to activate, stopping now!"
+	popd || exit 1
+	exit 1
 fi
 
 # You can copy this file to 'hydrus_client-user.sh' and add in your own launch parameters here if you like. A git pull won't overwrite that filename.
@@ -31,8 +31,7 @@ fi
 # unset WAYLAND_DISPLAY
 #
 
-python hydrus_client.py -d="/run/media/lan/03f3a4d6-d684-4825-b40d-4fc6afa4e176/data/hydrus/db/" "$@"
-
+python hydrus_client.py -d="/home/lan/Datos/WDC/data/hydrus/db/" "$@"
 
 deactivate
 
